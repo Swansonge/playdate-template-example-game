9cd405a149f2a53d9397101d057361b9596be56d
Eric Swanson <51669372+Swansonge@users.noreply.github.com>
2025-04-02 16:50:57 -0700

    Add horizontal movement, update project files

--- README.md
+++
@@ -1,9 +0,0 @@
-# Playdate Template Example Game
-This is a small example game to dip your toes into Playdate game development after completing the [Playdate template](https://github.com/SquidGodDev/playdate-template/tree/main) environment setup. The game is a simple one where you control a capybara with the crank to dodge rocks. The `main.lua` file contains the final project. The implementation of the game code is broken up into steps in the files labeled `step1` through `step9`. You can verify the accuracy of your code at each step by copying and pasting your code and the step code in [DiffChecker](https://www.diffchecker.com/).
-
-<p align="center">
-  <img src="https://github.com/user-attachments/assets/87caf881-192e-4c59-9169-3c66edb9ad63" alt="GIF of gameplay"/>
-</p>
-
-# License
-This project is licensed under the MIT license.
--- source/main.lua
+++ source/main.lua
@@ -1,5 +1,3 @@
--- Step 8: Ramp obstacle speed
-
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 
@@ -9,11 +7,14 @@ local gfx = pd.graphics
 -- Player
 local playerStartX = 40
 local playerStartY = 120
-local playerSpeed = 3
+local playerVertSpeed = 3
+local playerHorizSpeed = 3
 local playerImage = gfx.image.new("images/capybara")
 local playerSprite = gfx.sprite.new(playerImage)
 playerSprite:setCollideRect(4, 4, 56, 40)
+-- move player to position at start of game
 playerSprite:moveTo(playerStartX, playerStartY)
+-- add player sprite to sprite draw list 
 playerSprite:add()
 
 -- Game State
@@ -32,31 +33,53 @@ obstacleSprite:add()
 function pd.update()
     gfx.sprite.update()
 
+    -- start the game on player input
     if gameState == "stopped" then
+
         gfx.drawTextAligned("Press A to Start", 200, 40, kTextAlignment.center)
         if pd.buttonJustPressed(pd.kButtonA) then
             gameState = "active"
+
+            -- reset score and speed
             score = 0
             obstacleSpeed = 5
+
             playerSprite:moveTo(playerStartX, playerStartY)
             obstacleSprite:moveTo(450, math.random(40, 200))
         end
+
     elseif gameState == "active" then
+
+        -- crank controls vertical position
         local crankPosition = pd.getCrankPosition()
         if crankPosition <= 90 or crankPosition >= 270 then
-            playerSprite:moveBy(0, -playerSpeed)
+            playerSprite:moveBy(0, -playerVertSpeed)
         else
-            playerSprite:moveBy(0, playerSpeed)
+            playerSprite:moveBy(0, playerVertSpeed)
+        end
+        
+        -- d-pad controls horizontal position
+        local rightPressed = pd.buttonIsPressed(pd.kButtonRight)
+        local leftPressed = pd.buttonIsPressed(pd.kButtonLeft)
+        if rightPressed then
+            playerSprite:moveBy(playerHorizSpeed, 0)
+        elseif leftPressed then
+            playerSprite:moveBy(-playerHorizSpeed, 0)
         end
 
-        local actualX, actualY, collisions, length = obstacleSprite:moveWithCollisions(obstacleSprite.x - obstacleSpeed, obstacleSprite.y)
+        local actualX, actualY, collisions, length =  obstacleSprite:moveWithCollisions(obstacleSprite.x - obstacleSpeed, obstacleSprite.y)
+        --reset obstacle to right side (offscreen) if it goes left offscreen
         if obstacleSprite.x < -20 then
             obstacleSprite:moveTo(450, math.random(40, 200))
             score += 1
+            -- make game harder as score gets higher
             obstacleSpeed += 0.2
+            print(obstacleSpeed)
         end
 
-        if length > 0 or playerSprite.y > 270 or playerSprite.y < -30 then
+        -- check if player moves out of bounds or collides with obstacle
+        --because there is only 1 obstacle, length of collision list will be 1
+        if length > 0 or playerSprite.y > 270 or playerSprite.y < -30 or playerSprite.x > 430 or playerSprite.x < -30 then
             gameState = "stopped"
         end
     end
